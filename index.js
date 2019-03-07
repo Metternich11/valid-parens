a87b31f5e5aa994c95b5765c5aeddab1538dc74c
Felix Weinberger <felix.weinberger@gmail.com>
2019-03-07 09:36:02 +0100

    feat(tp): add initial version of tests, readme to do

---
+++ index.js
@@ -0,0 +1,35 @@
+
+// Valid parentheses (20 mins)
+
+// CHALLENGE
+
+// Write a function called validParens that determines if 
+// the order of the parentheses in a string is valid. The function 
+// should return true if the string is valid, and `false` if it's invalid.
+
+// Your input 'parens' is guaranteed to be a string that contains only
+// a series of '(' or ')'. No other characters will be passed.
+
+// EXAMPLES
+
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+
+// TESTING YOUR SOLUTION
+
+// To test your solution, run 'npm install' in the root directory
+// and then run 'npm test' to run the automated tests.
+
+// SUBMITTING YOUR SOLUTION
+
+// When done, make sure you're working on a forked repo, push your
+// changes to your forked repo and submit a pull request.
+// Alternatively send an email with your solution to felix.weinberger@gmail.com
+
+function validParens (parens) {
+  // your code here
+}
+
+module.exports = validParens;
